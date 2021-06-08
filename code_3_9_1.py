315e39d1844bba25c64785a8f8ef70e4dbec9191
Sultan <sultan@MacBook-Pro.local>
2021-06-08 20:14:16 +0600

    second commit

--- code_3_9_1.py
+++ code_3_9_1.py
@@ -43,6 +43,22 @@ def get_html(url):
     return r.text
 
 
+def get_data(html, url):
+    soup = bs.BeautifulSoup(html, 'html.parser')
+    category = soup.find('ul', class_='breadcrumb2').find_all('li')[1].a.text
+    ads = soup.find('div', class_='list-view').find_all('div', class_='item')
+    products = []
+    for ad in ads:
+        common_list = []
+        title = ad.find('div', class_='listbox_title')
+        link = ad.find('div', class_='listbox_title').a.get('href')
+        common_list.append(title.text.replace('\n', ''))
+        common_list.append('https://www.kivano.kg' + link)
+        common_list.append(category)
+        products.append(Product(common_list[0], common_list[1], common_list[2]))
+    return products
+
+
 def get_total_pages(html):
     soup = bs.BeautifulSoup(html, 'html.parser')
     pages = soup.find('ul', class_='pagination').find_all('a')[2].get('href')
@@ -50,124 +66,30 @@ def get_total_pages(html):
     return int(total_pages)
 
 
-def get_data_1():
-    base_url = 'https://www.kivano.kg/kompyutery?'
-    url = 'https://www.kivano.kg/kompyutery?page=1'
+def get_urls(base_urls, urls):
+    total_page = get_total_pages(get_html(urls))
     page_part = 'page='
-    total_page = get_total_pages(get_html(url))
-    products = []
-
     for i in range(1, total_page + 1):
-        urls = base_url + page_part + str(i)
-        html = get_html(urls)
-        soup = bs.BeautifulSoup(html, 'html.parser')
-
-        category = soup.find('ul', class_='breadcrumb2').find_all('li')[1].a.text
-        ads = soup.find('div', class_='list-view').find_all('div', class_='item')
-
-        for ad in ads:
-            common_list = []
-            title = ad.find('div', class_='listbox_title')
-            link = ad.find('div', class_='listbox_title').a.get('href')
-            common_list.append(title.text.replace('\n', ''))
-            common_list.append('https://www.kivano.kg' + link)
-            common_list.append(category)
-            products.append(Product(common_list[0], common_list[1], common_list[2]))
-    return products
-get_data_1()
-
-def get_data_2():
-    base_url = 'https://www.kivano.kg/avtotovary?'
-    url = 'https://www.kivano.kg/avtotovary?page=1'
-    page_part = 'page='
-    total_page = get_total_pages(get_html(url))
-    products = []
-
-    for i in range(1, total_page + 1):
-        urls = base_url + page_part + str(i)
-        html = get_html(urls)
-        soup = bs.BeautifulSoup(html, 'html.parser')
-
-        category = soup.find('ul', class_='breadcrumb2').find_all('li')[1].a.text
-        ads = soup.find('div', class_='list-view').find_all('div', class_='item')
-
-        for ad in ads:
-            common_list = []
-            title = ad.find('div', class_='listbox_title')
-            link = ad.find('div', class_='listbox_title').a.get('href')
-            common_list.append(title.text.replace('\n', ''))
-            common_list.append('https://www.kivano.kg' + link)
-            common_list.append(category)
-            products.append(Product(common_list[0], common_list[1], common_list[2]))
-    return products
-get_data_2()
+        all_urls = base_urls + page_part + str(i)
+        advertisements_1 = Advertisements(get_data(get_html(all_urls), base_urls))
+        writer_1 = Writer(advertisements_1)
+        writer_1.write_to_csv()
 
 
-def get_data_3():
-    base_url = 'https://www.kivano.kg/elektronika?'
-    url = 'https://www.kivano.kg/elektronika?page=1'
-    page_part = 'page='
-    total_page = get_total_pages(get_html(url))
-    products = []
+def main():
+    base_url_1 = 'https://www.kivano.kg/kompyutery?'
+    url_1 = 'https://www.kivano.kg/kompyutery?page=1'
+    get_urls(base_url_1, url_1)
+    base_url_2 = 'https://www.kivano.kg/avtotovary?'
+    url_2 = 'https://www.kivano.kg/avtotovary?page=1'
+    get_urls(base_url_2, url_2)
+    base_url_3 = 'https://www.kivano.kg/elektronika?'
+    url_3 = 'https://www.kivano.kg/elektronika?page=1'
+    get_urls(base_url_3, url_3)
+    base_url_4 = 'https://www.kivano.kg/odezhda-i-aksessuary?'
+    url_4 = 'https://www.kivano.kg/odezhda-i-aksessuary?page=1'
+    get_urls(base_url_4, url_4)
 
-    for i in range(1, total_page + 1):
-        urls = base_url + page_part + str(i)
-        html = get_html(urls)
-        soup = bs.BeautifulSoup(html, 'html.parser')
-
-        category = soup.find('ul', class_='breadcrumb2').find_all('li')[1].a.text
-        ads = soup.find('div', class_='list-view').find_all('div', class_='item')
-
-        for ad in ads:
-            common_list = []
-            title = ad.find('div', class_='listbox_title')
-            link = ad.find('div', class_='listbox_title').a.get('href')
-            common_list.append(title.text.replace('\n', ''))
-            common_list.append('https://www.kivano.kg' + link)
-            common_list.append(category)
-            products.append(Product(common_list[0], common_list[1], common_list[2]))
-    return products
-get_data_3()
-
-
-def get_data_4():
-    base_url = 'https://www.kivano.kg/odezhda-i-aksessuary?'
-    url = 'https://www.kivano.kg/odezhda-i-aksessuary?page=1'
-    page_part = 'page='
-    total_page = get_total_pages(get_html(url))
-    products = []
-
-    for i in range(1, total_page + 1):
-        urls = base_url + page_part + str(i)
-        html = get_html(urls)
-        soup = bs.BeautifulSoup(html, 'html.parser')
-
-        category = soup.find('ul', class_='breadcrumb2').find_all('li')[1].a.text
-        ads = soup.find('div', class_='list-view').find_all('div', class_='item')
-
-        for ad in ads:
-            common_list = []
-            title = ad.find('div', class_='listbox_title')
-            link = ad.find('div', class_='listbox_title').a.get('href')
-            common_list.append(title.text.replace('\n', ''))
-            common_list.append('https://www.kivano.kg' + link)
-            common_list.append(category)
-            products.append(Product(common_list[0], common_list[1], common_list[2]))
-    return products
-get_data_4()
 
 if __name__ == '__main__':
-    advertisements_1 = Advertisements(get_data_1())
-    advertisements_2 = Advertisements(get_data_2())
-    advertisements_3 = Advertisements(get_data_3())
-    advertisements_4 = Advertisements(get_data_4())
-
-
-    writer_1 = Writer(advertisements_1)
-    writer_2 = Writer(advertisements_2)
-    writer_3 = Writer(advertisements_3)
-    writer_4 = Writer(advertisements_4)
-
-    writer_list = [writer_1, writer_2, writer_3, writer_4]
-    for writer in writer_list:
-        writer.write_to_csv()
+    main()
